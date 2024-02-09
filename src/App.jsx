aafbb9939c14574ebca0edcd581f4b52b615f681
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-09 06:36:20 +0300

    implement useRef and use state to display the results.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import { Form } from 'react-router-dom'
+import { useRef, useState } from 'react'
 
-const App = () => (
-  <>
-    <Form />
-  </>
-)
+const App = () => {
+  const [search, setSearch] = useState('')
+  const searchContainer = useRef(null)
+
+  console.log('Search ', searchContainer.current && searchContainer.current.value);
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="d-flex flex-wrap justify-content-evenly">
+          <div className='w-50 my-5 d-flex'>
+            <div className="form-floating">
+              <input
+                type="search"
+                name='username'
+                id='username'
+                className="form-control"
+                placeholder='John Doe'
+                ref={searchContainer}
+              />
+              <label htmlFor="username" className="w-100 bg-transparent text-secondary"> search for items... </label>
+            </div>
+            <button type="button" className="btn btn-primary btn-md mx-2" onClick={() => setSearch(searchContainer.current.value)}> Search </button>
+          </div>
+        </div>
+        <h1>Searching for {search || 'nothing'}</h1>
+      </div>
+    </div>
+  )
+}
 
 export default App
